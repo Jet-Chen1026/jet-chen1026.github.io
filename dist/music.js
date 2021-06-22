8a98602cfa640d04d9b1ae31d644be1a8a691522
jetchen <chenzhitong1026@gmail.com>
2021-06-22 18:32:50 +0800

    Site updated: 2021-06-22 18:32:50

--- dist/music.js
+++ dist/music.js
@@ -1,11 +1,17 @@
+
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
+    autoplay: true,
+    loop: 'all',
+    volume: 0.7,
+    listFolded: true,
+    listMaxHeight: 60,
     audio: [
-	{
-        name: 'The truth that you leave',      #歌名
-        artist: 'piano boy',  #歌手
-        url: 'https://music.163.com/#/song?id=410518453',    #mp3的url地址
-        cover: 'http://p1.music.126.net/y_m2FXuYNBxITD4D80Aq2w==/1372190524437978.jpg' #歌的封面
-    }
-	]
+        {
+            name: 'The truth that you leave',
+        	artist: 'piano boy',
+        	url: 'http://bd.kuwo.cn/play_detail/57755407',
+        	cover: 'http://p1.music.126.net/y_m2FXuYNBxITD4D80Aq2w==/1372190524437978.jpg'
+        }
+    ]
 });
